e5f736939be19cbd7e6a766ef3a9eb4171a33e7f
Nathan-Nsa <nathan.codedev@gmail.com>
2023-02-22 13:58:17 +0100

    Implemented Geography Page

--- server/routes/client.js
+++ server/routes/client.js
@@ -1,10 +1,11 @@
-import express from 'express';
-import {getProducts, getCustomers, getTransactions} from '../controllers/client.js'
+import express, { Router } from 'express';
+import {getProducts, getCustomers, getTransactions, getGeogrpahy} from '../controllers/client.js'
 
 const router = express.Router();
 
 router.get("/products", getProducts);
 router.get("/customers", getCustomers);
-router.get("/transactions", getTransactions)
+router.get("/transactions", getTransactions);
+router.get("/geography", getGeogrpahy);
 
 export default router;
